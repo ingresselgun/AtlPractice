388329e763cd11232899f3397dbc336396678226
ingresselgun <ingresselgun010@gmail.com>
2024-03-19 17:01:35 +0400

    solved issues

--- Lesson2/Car.java
+++ Lesson2/Car.java
@@ -8,17 +8,6 @@ public class Car {
         // create variables
         int a = 7, b = 11;
 
-        // value of a and b
-        System.out.println("a is " + a + " and b is " + b);
-
-        // == operator
-        System.out.println(a == b);  // false
-
-        // != operator
-        System.out.println(a != b);  // true
-
-        // > operator
-        System.out.println(a > b);  // false
 
         // < operator
         System.out.println(a < b);  // true
