5ce17447fae69cdb3328c08b9a8a57a9aa1f50a2
ingresselgun <ingresselgun010@gmail.com>
2024-03-19 16:56:40 +0400

    I have implemented Car class with math operators

---
+++ Lesson1/Main.java
@@ -0,0 +1,26 @@
+package Lesson1;
+
+class Main {
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Welcome to our App!");
+        System.out.println(5/0);
+
+
+
+    }
+
+
+
+}
+
+
+
